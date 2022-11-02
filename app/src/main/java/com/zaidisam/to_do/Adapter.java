d43f4d0b25347f57a4a10970904f814ccdfed00f
Sameer Zaidi <zaidisameer02@gmail.com>
2022-11-02 16:54:32 +0530

    Final To-do Commit

---
+++ app/src/main/java/com/zaidisam/to_do/Adapter.java
@@ -0,0 +1,118 @@
+package com.zaidisam.to_do;
+
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter <Adapter.viewholder>{
+    private DatabaseReference tododata;
+
+
+   Context context;
+   ArrayList<ToDoModel> arrayList;
+   public String id;
+
+     Adapter(Context context , ArrayList<ToDoModel> arrayList)
+     {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.todolayout,parent,false);
+        return new viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+         int status=0;
+        String android_id = Settings.Secure.getString(this.context.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+         tododata = FirebaseDatabase.getInstance().getReference(android_id).child("Tasks").child(arrayList.get(position).key);
+             holder.task.setText(arrayList.get(position).task);
+             holder.date.setText("Due Date: "+arrayList.get(position).date);
+             holder.priority.setText("Priority: "+arrayList.get(position).priority);
+             status= arrayList.get(position).status;
+             id = arrayList.get(position).key.toString();
+           if (status== 1) {
+            holder.chkbox.setChecked(true);
+            holder.task.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            holder.priority.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            holder.date.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+               holder.chkbox.setChecked(false);
+
+               holder.chkbox.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       tododata = FirebaseDatabase.getInstance().getReference(android_id).child("Tasks").child(arrayList.get(position).key);
+                       if (holder.chkbox.isChecked()) {
+                           Toast.makeText(context, "Yayy....Task Completed", Toast.LENGTH_SHORT).show();
+                           tododata.child("status").setValue(1);
+                           holder.task.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                           holder.priority.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                           holder.date.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                       } else
+                           tododata.child("status").setValue(0);
+
+                   }
+               });
+           }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tododata = FirebaseDatabase.getInstance().getReference(android_id).child("Tasks").child(arrayList.get(position).key);
+                Toast.makeText(context,"Task Deleted",Toast.LENGTH_SHORT).show();
+                id = arrayList.get(position).key.toString();
+                tododata.removeValue();
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        TextView task,date,priority;
+        CheckBox chkbox;
+        int status;
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            task=(TextView)itemView.findViewById(R.id.chkboxtxt);
+            date = itemView.findViewById(R.id.date);
+            chkbox = itemView.findViewById(R.id.chkboxtxt);
+            priority = itemView.findViewById(R.id.priority);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/zaidisam/to_do/MainActivity.java
@@ -0,0 +1,135 @@
+package com.zaidisam.to_do;
+
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+     public  RecyclerView rcv;
+   public FloatingActionButton fab;
+   public CheckBox chkbox;
+    private DatabaseReference tododata;
+   ArrayList<ToDoModel> arrtodo= new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        String android_id = Settings.Secure.getString(this.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rcv = findViewById(R.id.rcylview);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+           fab = findViewById(R.id.fab);
+           fab.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  addnote();
+              }
+          });
+        tododata= FirebaseDatabase.getInstance().getReference(android_id).child("Tasks");
+
+
+               tododata.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String task,date,priority;
+                int status;
+                arrtodo.clear();
+                for(DataSnapshot snap: snapshot.getChildren())
+                {
+                    ToDoModel data = snap.getValue(ToDoModel.class);
+                    task = data.getTask().toString();
+                    date = data.getDate().toString();
+                    priority = data.getPriority().toString();
+                   Log.d(TAG, snap.getKey());
+                    Adapter adapter = new Adapter(MainActivity.this,arrtodo);
+                    rcv.setAdapter(adapter);
+
+                    arrtodo.add(new ToDoModel(task,date,priority,data.getStatus(),snap.getKey()));
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+    }
+
+    private void addnote() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater =LayoutInflater.from(this);
+        View myView =inflater.inflate(R.layout.input_layout,null);
+        myDialog.setView(myView);
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        final Spinner itemSpinner= myView.findViewById(R.id.itemsspinner);
+        final EditText task = myView.findViewById(R.id.task);
+        final Button cancel = myView.findViewById(R.id.cancel);
+        final Button save = myView.findViewById(R.id.save);
+        final EditText datetim = myView.findViewById(R.id.datetime);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tasktxt = task.getText().toString();
+                String p = itemSpinner.getSelectedItem().toString();
+                String datetime = datetim.getText().toString();
+
+                String id = tododata.push().getKey();
+                ToDoModel data = new ToDoModel(tasktxt,datetime,p,0,id);
+                tododata.child(id).setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+                dialog.dismiss();
+
+
+            }
+        });
+        dialog.show();
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+}
